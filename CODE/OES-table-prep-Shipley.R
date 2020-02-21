b2173a548c3e9bf0a78364daad1b0830efed256e
David Herzberg <dherzberg@wpspublish.com>
2020-02-21 15:54:34 -0500

    start on final OES table assembly

--- CODE/OES-table-prep-Shipley.R
+++ CODE/OES-table-prep-Shipley.R
@@ -6,6 +6,9 @@ form <- c('child', 'adult')
 scale_acr <- c('VOC', 'ABS', 'BLO', 'CMA', 'CMB')
 CV <- c('68', '90', '95')
 
+# read in percentile lookup column
+perc_lookup <- suppressMessages(read_csv(here('INPUT-FILES/SHIPLEY/Percentile-Lookup-SS.csv')))
+
 scale_readin <- function(x) {
   # express the directory paths to the input files as a char vec.
   here(
@@ -30,11 +33,7 @@ scale_lookup_pre <- form %>%
   bind_rows(.id = 'form')
 
 # Read in age equiv .cvs
-age_equiv_pre <- read_csv(here('INPUT-FILES/SHIPLEY/age-equiv.csv'))
-
-# join scale, AE
-all_lookup <- scale_lookup_pre %>% 
-  left_join(age_equiv_pre, by = 'rawscore')
+age_equiv_pre <- suppressMessages(read_csv(here('INPUT-FILES/SHIPLEY/age-equiv.csv')))
 
 # Read in CV .xlsx
 CV_readin <- function(x) {
@@ -57,20 +56,108 @@ CV_lookup_pre <- CV %>%
   setNames(CV) %>% 
   reduce(full_join, by = c('form', 'agestrat'))
 
-######### NOTHING BELOW THIS LINE HAS BEEN MODDED FOR SHIPLEY
-
-# Read in growth score .xlsx
-growth_lookup_pre <- here('INPUT-FILES/growth.xlsx') %>% 
-  excel_sheets() %>%
-  set_names() %>%
-  map_df(read_excel,
-         path = here('INPUT-FILES/growth.xlsx'),
-         .id = 'form')
-
-# join scale, CV, growth
+# join scale, AE, CV
 all_lookup <- scale_lookup_pre %>% 
-  left_join(CV_lookup_pre, by = c('form', 'agestrat')) %>% 
-  left_join(growth_lookup_pre, by = c('form', 'rawscore'))
+  left_join(age_equiv_pre, by = 'rawscore') %>% 
+  left_join(CV_lookup_pre, by = c('form', 'agestrat'))
 
+# Create CIs and transform into multilevel format required for final OES output.
+all_lookup_tall <- scale_acr %>%
+  map_dfc(~ all_lookup %>% 
+            # dplyr::transmute() is similar to mutate(), but it drops the input
+            # columns after creating the new var
+            transmute(
+              # Next four operations lines get upper, lower bounds of CIs as numbers
+              !!str_c(.x, '_CI68_LB_pre') := !!sym(.x) - !!sym(str_c(.x, '_CV68')),
+              !!str_c(.x, '_CI68_UB_pre') := !!sym(.x) + !!sym(str_c(.x, '_CV68')), 
+              !!str_c(.x, '_CI90_LB_pre') := !!sym(.x) - !!sym(str_c(.x, '_CV90')),
+              !!str_c(.x, '_CI90_UB_pre') := !!sym(.x) + !!sym(str_c(.x, '_CV90')), 
+              !!str_c(.x, '_CI95_LB_pre') := !!sym(.x) - !!sym(str_c(.x, '_CV95')),
+              !!str_c(.x, '_CI95_UB_pre') := !!sym(.x) + !!sym(str_c(.x, '_CV95')), 
+              # Next four operations truncate UB at 160, LB at 40, and coerce
+              # both to character
+              !!str_c(.x, '_CI68_LB') := as.character(case_when(
+                !!sym(str_c(.x, '_CI68_LB_pre')) < 40 ~ 40,
+                TRUE ~ !!sym(str_c(.x, '_CI68_LB_pre'))
+              )),
+              !!str_c(.x, '_CI68_UB') := as.character(case_when(
+                !!sym(str_c(.x, '_CI68_UB_pre')) > 160 ~ 160,
+                TRUE ~ !!sym(str_c(.x, '_CI68_UB_pre'))
+              )),
+              !!str_c(.x, '_CI90_LB') := as.character(case_when(
+                !!sym(str_c(.x, '_CI90_LB_pre')) < 40 ~ 40,
+                TRUE ~ !!sym(str_c(.x, '_CI90_LB_pre'))
+              )),
+              !!str_c(.x, '_CI90_UB') := as.character(case_when(
+                !!sym(str_c(.x, '_CI90_UB_pre')) > 160 ~ 160,
+                TRUE ~ !!sym(str_c(.x, '_CI90_UB_pre'))
+              )),
+              !!str_c(.x, '_CI95_LB') := as.character(case_when(
+                !!sym(str_c(.x, '_CI95_LB_pre')) < 40 ~ 40,
+                TRUE ~ !!sym(str_c(.x, '_CI95_LB_pre'))
+              )),
+              !!str_c(.x, '_CI95_UB') := as.character(case_when(
+                !!sym(str_c(.x, '_CI95_UB_pre')) > 160 ~ 160,
+                TRUE ~ !!sym(str_c(.x, '_CI95_UB_pre'))
+              )),
+              # Next two operations yield the formatted, truncated CIs as strings
+              !!str_c(.x, '_CI68') :=
+                str_c(!!sym(str_c(.x, '_CI68_LB')), !!sym(str_c(.x, '_CI68_UB')), sep = ' - '),
+              !!str_c(.x, '_CI90') :=
+                str_c(!!sym(str_c(.x, '_CI90_LB')), !!sym(str_c(.x, '_CI90_UB')), sep = ' - '),
+              !!str_c(.x, '_CI95') :=
+                str_c(!!sym(str_c(.x, '_CI95_LB')), !!sym(str_c(.x, '_CI95_UB')), sep = ' - ')
+            )
+  ) %>%
+  # At this point the object has only the new columns; all input columns have
+  # been dropped by transmute(). Now bind_cols joins the new cols with the
+  # original input set. select() then pares to only those columsn needed in the
+  # final OES output.
+  bind_cols(all_lookup, .) %>% 
+  select(form:last(scale_acr), ends_with('CI68'), ends_with('CI90'), ends_with('CI95'), ends_with('_AE')) %>% 
+  # rename SS cols so all cols to be gathered are named with the format
+  # "scaleName_scoreType"
+  rename_at(vars(scale_acr), ~ paste0(.x,"_SS")) %>% 
+  # gather "scaleName_scoreType" cols into key column, SS and CI values into val
+  # col
+  gather(key, val, 4:ncol(.)) %>%
+  # Now split "scaleName_scoreType" in key col into two cols: scale and type
+  extract(key, into = c("scale", "type"), "([:alpha:]{3})?\\_?(.*)") %>%
+  # spread so that type yields cols of SS, G, CI90, CI95, and that quad remains
+  # paired with correct form, agestrat, rawscore, and scale.
+  spread(type, val) %>% 
+  select(scale, form, agestrat, rawscore, SS, CI90, CI95, AE) %>% 
+  rename(AgeEquiv = AE) %>% 
+  arrange(scale) %>% 
+  mutate(
+    SS = as.numeric(SS)
+  )
+
+# Assemble OES output table: first stack scale and GDS tables
+OES_lookup <- all_lookup_tall %>% 
+  # This drops rows that are NA on SS, which shouldn't exist on final output table.
+  filter(!is.na(SS)) %>% 
+  left_join(perc_lookup, by = 'SS') %>% 
+  
+  ################### START HERE
+  
+  
+  mutate(descrange = case_when(
+    SS >= 131 ~ 'Well above average',
+    between(SS, 116, 130) ~ 'Above average',
+    between(SS, 85, 115) ~ 'Average',
+    between(SS, 70, 84) ~ 'Below average',
+    SS <= 69 ~ 'Delayed',
+    TRUE ~ NA_character_
+  )) %>% 
+  arrange(match(scale, c('PHY', 'ADP', 'SOC', 'COG', 'COM', 'GDS')), form, agestrat) %>% 
+  left_join(age_labels, by = 'agestrat') %>% 
+  rename(agerange = OES_label) %>% 
+  select(scale, form, agerange, rawscore, SS, CI90, CI95, growth, descrange, Percentile, AgeEquiv)
+
+# Write OES lookup table to .csv
+write_csv(OES_lookup, here(
+  'OUTPUT-FILES/DP4-OES-lookup.csv'
+))
 
 
